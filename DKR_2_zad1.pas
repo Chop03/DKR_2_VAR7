1e76646cf197f3e090371c95c18bfc15b02c1b86
Chop03 <144237862+Chop03@users.noreply.github.com>
2023-12-19 11:09:52 +0300

    Add files via upload

---
+++ DKR_2_zad1.pas
@@ -0,0 +1,32 @@
+﻿program fibonachi;
+var f1, f2, z, n, a:integer;
+arr1, arr2: array of integer;
+flag : boolean;
+begin
+writeln('Введите число чисел последовательности: ');
+read(a);
+writeln('Вводите числа: ');
+setlength(arr1, a);
+setlength(arr2, a);
+for i:integer:= 0 to a-1 do
+begin
+readln(n);
+arr2[i]:=n;
+end;
+f1:=arr2[0]; arr1[0]:=f1;
+f2:=arr2[1]; arr1[1]:=f2;
+z:=f1+f2;
+for i:integer:=2 to a-1 do
+begin
+arr1[i]:=z;
+f1:=f2;
+f2:=z;
+z := f1+f2;
+end;
+
+
+flag := True;
+for i:integer:=0 to a-1 do
+if arr1[i] <> arr2[i] then begin flag := false; break; end;
+if flag = True then writeln('Соответствует последовательности') else writeln('Не соответствует последовательности');
+end.
---
+++ DKR_2_zad2.pas
@@ -0,0 +1,32 @@
+﻿var s,ps,new_s: string; poz,i,j : integer; f,p : boolean;
+ST : array of char;
+
+begin
+  
+  readln(s); // ввод строки
+  readln(ps); // ввод подстроки
+  
+  for i := 1 to length(s) do begin // вычисление позиций подстроки
+    if s[i] = ps[1] then for j := 1 to length(ps) do begin
+      f := True;
+      if s[i+j-1] <> ps[j] then begin f := False; break; end;
+    end;
+    if f = True then begin poz := i; break; end;
+  end;
+  
+  SetLength(ST,length(s)+1); // создание и установление длины массива ST
+  for i := 0 to length(s)-1 do ST[i] := s[i+1]; // ввод в массив ST строку s
+  ST[length(s)] := ' '; // пробел в конец массива чтобы считывал в след. циклу
+  
+  for i := 1 to poz-1 do new_s := new_s + ST[i-1]; // ввод в новую строку до подстроки
+  
+  for i := poz-1 to poz+length(ps)-2 do begin // ввод подстроки без вторых вхождений
+    if p = False then new_s := new_s + ST[i];
+    if (ST[i] = ' ') and (p = True) then begin p := False; ST[i] := '.' end;
+    if (ST[i] = ' ') and (p = False) then p := True;
+  end;
+  
+  for i := poz+length(ps)+1 to length(s) do new_s := new_s + ST[i-1]; // ввод в новую строку после подстроки
+  
+  writeln(new_s) // вывод новой строки без каждых вторых вхождений в заданной подстроке
+end.
